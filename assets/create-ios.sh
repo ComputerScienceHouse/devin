d9cde7df2b4148eac386c5389f972cc8edec1476
Mary Strodl <ipadlover8322@gmail.com>
2024-01-20 13:25:22 -0500

    Dumpin

--- assets/create-ios.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-convert icon-android.png -background "#E11C52" -flatten icon-ios.png
--- build_wearos.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-flutter build appbundle --release --flavor wearos --dart-define=edu.rit.csh.devin.flavor=wearos
